e46b277d73bbd1209fbb645356a8ea6228abca5f
Theraot <Theraot@gmail.com>
2023-07-12 17:28:03 -0500

    Fix unnecessarly flagging the project as modified

--- addons/ActionIcon/plugin.gd
+++ addons/ActionIcon/plugin.gd
@@ -2,9 +2,14 @@
 extends EditorPlugin
 
 
+const autoload_name := "__action_texture_picker"
+
+
 func _enter_tree():
 	var base_path := (get_script() as Script).resource_path.get_base_dir()
-	add_autoload_singleton("__action_texture_picker", base_path.path_join("action_texture_picker.gd"))
+	await get_tree().process_frame
+	if not is_instance_valid(get_tree().root.get_node_or_null(autoload_name)):
+		add_autoload_singleton("__action_texture_picker", base_path.path_join("action_texture_picker.gd"))
 
 
 func _exit_tree():
